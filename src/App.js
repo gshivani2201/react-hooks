6b9a5a0650c81e1f3f502e44dc9f1b36d6933a6f
Shivani Gupta <gupta.shivani7898@gmail.com>
2022-05-14 16:39:17 +0530

    useRef Hook P-1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import React from 'react'
-import Counter from './components/Counter';
+import FocusInput from './components/FocusInput';
 
 function App() {
 
   return (
       <div className="App">
-        <Counter />
+        <FocusInput />
       </div>
   );
 }
