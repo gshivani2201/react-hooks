4875f324ab7741852d5fad669319a04b3c754974
Shivani Gupta <gupta.shivani7898@gmail.com>
2022-05-14 09:53:22 +0530

    useReducer Hook with simple state and action

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
 import React from 'react'
-import CompC from './components/CompC';
-
-export const UserContext = React.createContext()
-export const ChannelContext = React.createContext()
+import CounterOne from './components/CounterOne';
 
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value={'Shivani'}>
-        <ChannelContext.Provider value={'Shivuu ka channel'}>
-          <CompC />
-        </ChannelContext.Provider>
-      </UserContext.Provider>
+      <CounterOne />
     </div>
   );
 }
