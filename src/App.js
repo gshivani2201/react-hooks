efdf06317ec943e379e5e2a494c2675cac06d09d
Shivani Gupta <gupta.shivani7898@gmail.com>
2022-05-14 14:36:07 +0530

    Fetching data with useReducer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import './App.css';
-import React, {useReducer} from 'react'
-import ComponentA from './components/ComponentA';
-import ComponentB from './components/ComponentB';
-import ComponentC from './components/ComponentC';
-
-export const CountContext = React.createContext()
-
-const initialState = 0
-const reducer = (state, action) => {
-  switch(action) {
-    case 'increment':
-      return state + 1
-    case 'decrement' :
-      return state - 1
-    case 'reset':
-      return initialState
-    default:
-      return state
-  }
-}
+import React from 'react'
+import DataFetchingOne from './components/DataFetchingOne';
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState)
 
   return (
-    <CountContext.Provider value={{countState: count, countDispatch: dispatch}}>
       <div className="App">
-        App Count - {count}
-        <ComponentA />
-        <ComponentB />
-        <ComponentC />
+        <DataFetchingOne />
       </div>
-    </CountContext.Provider>
   );
 }
 
